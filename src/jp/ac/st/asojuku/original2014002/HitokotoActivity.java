3d3bdb270d085c633137d4fe9ec5f968d21cb662
student <student@PC111S36.fgakusei.local>
2014-06-09 14:30:18 +0900

    本日の提出（中編）

---
+++ src/jp/ac/st/asojuku/original2014002/HitokotoActivity.java
@@ -0,0 +1,20 @@
+package jp.ac.st.asojuku.original2014002;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HitokotoActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO 自動生成されたメソッド・スタブ
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.hyouji_activity);
+	}
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
--- src/jp/ac/st/asojuku/original2014002/MainActivity.java
+++ src/jp/ac/st/asojuku/original2014002/MainActivity.java
@@ -1,12 +1,11 @@
 package jp.ac.st.asojuku.original2014002;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 
 public class MainActivity extends Activity implements View.OnClickListener{
 
@@ -21,23 +20,34 @@ public class MainActivity extends Activity implements View.OnClickListener{
 		// TODO 自動生成されたメソッド・スタブ
 		super.onResume();
 		//ボタンをIDで探してボタン変数をつくる
-		Button btnOK = (Button)findViewById(R.id.btnOK);
+		Button btnOK = (Button)findViewById(R.id.btnOK3);
 		//ボタン変数にリスナーを登録する
 		btnOK.setOnClickListener(this);
+
+		// TODO 自動生成されたメソッド・スタブ
+		//ボタンをIDで探してボタン変数をつくる
+		Button btnOK2 = (Button)findViewById(R.id.btnOK2);
+		//ボタン変数にリスナーを登録する
+		btnOK2.setOnClickListener(this);
 	}
+
 
 	@Override
 	public void onClick(View v) {
 		// TODO 自動生成されたメソッド・スタブ
 		switch(v.getId()){ //どのボタンが押されたか判定
-			case R.id.btnOK: //btnMsgが押された
-				//エディットテキストから入力内容を取り出す
-				EditText etv = (EditText)findViewById(R.id.edtName);
-				String inputMsg = etv.getText().toString();
-
-				//メッセージ表示用のTextViewを探して、文字を設定
-				TextView tv = (TextView)findViewById(R.id.txvMsg);
-				tv.setText()
+			case R.id.btnOK3: //btnMsgが押された
+				// インテントのインスタンス生成
+				Intent intent = new Intent(MainActivity.this, HitokotoActivity.class);
+				// 次画面のアクティビティ起動
+				startActivity(intent);
+				break;
+			case R.id.btnOK2: //btnMsgが押された
+				// インテントのインスタンス生成
+				Intent intent2 = new Intent(MainActivity.this, MaintenanceActivity.class);
+				// 次画面のアクティビティ起動
+				startActivity(intent2);
+				break;
 		}
 	}
 
