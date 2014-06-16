bd6193a75db3efd0cb81b92c7b79c7bfa4e24b27
student <student@PC111S36.fgakusei.local>
2014-06-16 10:08:05 +0900

    本日の作業保存

--- src/jp/ac/st/asojuku/original2014002/MySQLiteOpenHelper.java
+++ src/jp/ac/st/asojuku/original2014002/MySQLiteOpenHelper.java
@@ -20,7 +20,7 @@ public class MySQLiteOpenHelper extends SQLiteOpenHelper {
 	@Override
 	public void onCreate(SQLiteDatabase db) {
 		// TODO 自動生成されたメソッド・スタブ
-		db.execSQL("CREATE TABLE IF NOT EXISTS" +
+		db.execSQL("CREATE TABLE IF NOT EXISTS " +
 				"Hitokoto (_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL , phrase TEXT)");
 	}
 
@@ -37,7 +37,7 @@ public class MySQLiteOpenHelper extends SQLiteOpenHelper {
 	 * @param inputMsg インサートするメッセージ
 	 */
 	public void insertHitokoto(SQLiteDatabase db, String inputMsg){
-		String sqlstr = "insert into HItokoto(phrase)values('"+inputMsg+"');";
+		String sqlstr = "insert into Hitokoto(phrase)values('"+inputMsg+"');";
 		try{
 			//トランザクション開始
 			db.beginTransaction();
